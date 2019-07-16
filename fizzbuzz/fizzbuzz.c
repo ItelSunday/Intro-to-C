d646ac4f5d2aebf12ce36aeca2ea1121f076d829
ItelSunday <iteldomingotech@gmail.com>
2019-07-15 20:23:52 -0700

    solved fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -9,30 +9,48 @@
     Don't forget to include newlines '\n' in your printf statements!
 */
 
+//---------------------------------------//
 // initialize a counter i=0
-// checks n times
-// increment a counter everytime it passes check
+// checks/test condition 
+// increment a loop everytime it passes check 
+//---------------------------------------//
 
 // If ith iteration divisible by 3, print "Fizz\n"
 // divisible by 5, print "Buzz\n"
 // divisible by both 3 and 5, print "FizzBuzz\n"
 
 // i++ every time that nothing gets printed and return counter
+//---------------------------------------//
 
-//Run in the terminal: gcc -Wall -Wextra -o fizzbuzz fizzbuzz.c then
-//Type ./fizzbuzz
+//---------------------------------------//
+//---------RUN IN THE TERMINAL-----------//
+// gcc -Wall -Wextra -o fizzbuzz fizzbuzz.c then,
+// ./fizzbuzz
+//---------------------------------------//
 
+// % = modulo is the remainder
+// 7 % 3 = 1
+// because the remainder of 7 / 3 is 1
+// 6 % 3 = 0 // no remainder
+// because the remainder of 6 / 3 is 0
+// i % z is the remainder of "i" divided by "z"
+//modulo is the remainder of the division of two numbers
+// 
 int fizzbuzz(int n)
 {
-    // int n;
+    for(int i=0; i<n; i++) {
+        if(i%3 == 0 && i%5 == 0) {
+			printf("%d FizzBuzz\n", i);
+        }
+        else if (i%3 == 0){
+            printf("%i Fizz\n", i);
+        }
+        else if(i%5 == 0){
+            printf("%i Buzz\n", i);
 
-    for(int i=0; i<10; i++) {
-        printf("%i is Fizz", i);
-        
+		}  
     }
     return 0;
-
-
 }
 
 #ifndef TESTING
