8b84efaa9d71cb47c2ad170ac18e1cd29698ac34
ItelSunday <iteldomingotech@gmail.com>
2019-07-15 17:05:31 -0700

    initial commit: started with fizz, wip

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -8,8 +8,30 @@
     every time that nothing gets printed and return the counter.
     Don't forget to include newlines '\n' in your printf statements!
 */
+
+// initialize a counter i=0
+// checks n times
+// increment a counter everytime it passes check
+
+// If ith iteration divisible by 3, print "Fizz\n"
+// divisible by 5, print "Buzz\n"
+// divisible by both 3 and 5, print "FizzBuzz\n"
+
+// i++ every time that nothing gets printed and return counter
+
+//Run in the terminal: gcc -Wall -Wextra -o fizzbuzz fizzbuzz.c then
+//Type ./fizzbuzz
+
 int fizzbuzz(int n)
 {
+    // int n;
+
+    for(int i=0; i<10; i++) {
+        printf("%i is Fizz", i);
+        
+    }
+    return 0;
+
 
 }
 
